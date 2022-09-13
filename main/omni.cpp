95ea196b61e3544d53338b7327e27f0f03209419
allgemein <amanetamada@gmail.com>
2022-09-13 23:13:30 +0900

    モータやコントローラの特性に関する調整ができるように修正

--- main/omni.cpp
+++ main/omni.cpp
@@ -4,29 +4,26 @@ Omni::Omni(Motor& motor1, Motor& motor2, Motor& motor3)
     : _motor1(motor1), _motor2(motor2), _motor3(motor3) {}
 void Omni::move(float vx, float vy, float vtheta) {
     float r = 0.0;
-    const int th = 50;
+    if (vx < _threshold) vx = 0;
+    if (vy < _threshold) vy = 0;
     // motor1
     // vx + rvtheta
     float v1 = vx + r * vtheta;
-    float speed1 = v1*2;
-    if(speed1<th) speed1 = 0;
+    float speed1 = v1 * 2 * _k_motor1;
     _motor3.setSpeed(speed1);
 
     // motor2
     // -(1/2)vx + (√3/2)vy + rvtheta
     float v2 = -0.5 * vx + (1.73 / 2) * vy + r * vtheta;
-    float speed2 = v2*2;
-    if(speed2<th) speed2 = 0;
+    float speed2 = v2 * 2 * _k_motor2;
     _motor1.setSpeed(speed2);
     // motor2
     // -(1/2)vx - √3/2vy + rvtheta
     float v3 = -0.5 * vx - (1.73 / 2) * vy + r * vtheta;
-    float speed3 = v3 * 2;
-    if(speed3<th) speed3 = 0;
+    float speed3 = v3 * 2 * _k_motor3;
     _motor2.setSpeed(speed3);
 
-     Serial.println(v1);
-    // Serial.print()
-     Serial.println(v2);
-     Serial.println(v3);
+    // Serial.println(v1);
+    // Serial.println(v2);
+    // Serial.println(v3);
 }
--- main/omni.h
+++ main/omni.h
@@ -6,8 +6,13 @@ class Omni {
     public:
     Omni(Motor& motor1, Motor& motor2, Motor& motor3);
     void move(float vx, float vy, float vtheta);
+    const float _k_motor1 = 1.0;
+    const float _k_motor2 = 1.0;
+    const float _k_motor3 = 1.0;
 
     private:
+    const int _threshold = 20;
+
     Motor& _motor1;
     Motor& _motor2;
     Motor& _motor3;
